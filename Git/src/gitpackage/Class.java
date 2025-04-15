34afc000fe7eca4c86b5258e489a74ccd2f9f9d7
admin <admin@DESKTOP-6PKG6CV>
2025-04-15 13:14:36 +0530

    comit

--- Git/src/gitpackage/Class.java
+++ Git/src/gitpackage/Class.java
@@ -3,7 +3,7 @@ package gitpackage;
 public class Class {
 
 	public static void main(String[] args) {
-		System.out.println("Total Number of Dropdown values");
+		System.out.println("Total Number of Dropdown value");
 	}
 
 }
