704422de004bf69d5fc90bb03829fba72c8a19d1
admin <admin@DESKTOP-6PKG6CV>
2025-04-15 13:39:13 +0530

    donee

--- Git/src/gitpackage/Class.java
+++ Git/src/gitpackage/Class.java
@@ -3,8 +3,8 @@ package gitpackage;
 public class Class {
 
 	public static void main(String[] args) {
-		System.out.println("Total Number of Dropdown value");
-		System.out.println("Done");
+		System.out.println("Total Number of Dropdown values");
+		
 	}
 
 }
