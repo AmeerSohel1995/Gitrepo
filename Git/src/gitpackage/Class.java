1c338878e30ac662c69c3ca7746c2a14bb312e01
admin <admin@DESKTOP-6PKG6CV>
2025-04-15 16:34:57 +0530

    remove some text

--- Git/src/gitpackage/Class.java
+++ Git/src/gitpackage/Class.java
@@ -4,7 +4,7 @@ public class Class {
 
 	public static void main(String[] args) {
 		System.out.println("Total Number of Dropdown values");
-		System.out.println("Total Number of Dropdown values");
+		System.out.println("s");
 	}
 
 }
