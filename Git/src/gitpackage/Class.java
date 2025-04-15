12604c7eb03d6a584e7cdf793fa70a0edad46aaa
Ameer Sohel <your_email@example.com>
2025-04-15 16:58:15 +0530

    added

--- Git/src/gitpackage/Class.java
+++ Git/src/gitpackage/Class.java
@@ -5,7 +5,7 @@ public class Class {
 	public static void main(String[] args) {
 		System.out.println("Total Number of Dropdown values");
 
-		System.out.println("s");
+		System.out.println("so");
 
 		
 		System.out.println("Total Number of checkboxes values");
